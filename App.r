471c16a75e23fbd9fd6385cddaa56b91a2578b66
AchrafBella <achraf.bella79@gmail.com>
2021-11-22 15:56:21 +0100

    new things

--- App.r
+++ App.r
@@ -1,98 +1,77 @@
 library(shiny)
-library(shinyWidgets)
-library(shinyalert)
+library(readr)
 library(dplyr)
 library(sf)
 library(ggplot2)
 
-source("data_acquisition.R")
-
-
-datasets = c('appearances', 'characters', 'episodes', 'populations', 'scenes')
 character_name_list = appearances %>% pull(name) %>% unique()
-colors =  c('red', 'blue', 'green')
-
-
-# The application
 
 ui <- fluidPage(
-  setBackgroundImage("background.jpg"),
-  useShinyalert(),
-  tags$audio(src = "got_opning.mp3", type = "audio/mp3", autoplay = T, controls = F),
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "style.css") 
-  ),
-  
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")),
   tags$header(
     tags$img(
       src = "ecc.png",
       title = "Ecole Centrale Casablanca",
       width = "70",
-      height = "55",
+      height = "45",
       class = "logo"
     ),
     tags$b("GoT Data Visualisation Application", class = "titre")
   ),
-
-
-  navbarPage('GOT visulization',
+  
+  navbarPage('Game of thrones visulization',
+             tabPanel("Got map",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("saison", "Choose a season",
+                            choices = c(1, 2, 3, 4, 5, 6, 7, 8),
+                          ),
+                          selectInput("episode", "Choose an Episode",
+                            choices = c(1,2,3,4,5),
+                          ),
+                          radioButtons(
+                            "ds",
+                            "See the place of :",
+                            choices = c("Scenes", "Death people")
+                          ),
+                        ),
+                        
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          plotOutput("distPlot")
+                        )
+                      )
+                      ),
              
-             tabPanel('GOT map', sidebarPanel(
-               selectInput("names", "Character name:",character_name_list),
-               selectInput("color", "Choose Color", colors,),
-             ), 
+             tabPanel("Time series data", sidebarPanel(
+               selectInput("names", "Choose character name:",character_name_list)),
              mainPanel(
-               h2("Spatial distribution of the selected person's scenes",
-               style='background-color:coral;padding-left: 15px'),
-               plotOutput(outputId = "map1", width = "100%"),
-             ) ),
+                 h2("Line graph of time spend per episode by a character",
+                    style='background-color:coral;padding-left: 15px'),
+                 plotOutput(outputId="g", width="300px",height="300px"),)),
              
-             tabPanel("Data Visualization", sidebarPanel(
-               selectInput("names1", "Character name:",character_name_list),
-               selectInput("color1", "Choose Color", colors,),
-               sliderInput("ep", "Episode number :",
-                           min = 2,
-                           max = 500,
-                           value = 5),
-             ), 
-             mainPanel(
-               basicPage(
-                 h2("Time spend per episode by a character",
-                                  style='background-color:coral;padding-left: 15px'),
-                        plotOutput(outputId="map2", width="300px",height="300px"),
-                 
-                 h2("Time spend per episode by a character",
-                              style='background-color:coral;padding-left: 15px'),
-                 plotOutput(outputId="map3", width="300px",height="300px")),
-               ),
-               
-
-             ),
              
-             tabPanel("Descriptive statistics",),
-             tabPanel("Correlation",),
-             
-             tabPanel("Data",  fluidRow(
+             tabPanel("Data", fluidRow(
                column(2,
                       selectInput(inputId="df",label="Select datasets",
-                                  choices = datasets),
+                      choices =  c('appearances', 'characters', 'episodes', 
+                      'populations', 'scenes')),
                ),
                column(10,  DT::dataTableOutput('rawtable'))
              ) ),
-  ),
-  
-  tags$br(),
-  tags$br(),
-  actionLink("aPropos", "About")
+             
+             tabPanel("About", includeHTML("www/a-propos.html"),br()),
+             tabPanel("Developers", includeHTML("www/Developers.html"),br()),
+  )            
 )
-
 
 server <- function(input, output){
   
   output$rawtable <- DT::renderDataTable(read_csv(
     file.path('data',paste(input$df,'.csv', sep=''))))
   
-  output$map1 <- renderPlot({
+  output$g <- renderPlot({
     
     character_duration= appearances %>% filter(name==input$names) %>% 
       left_join(scenes) %>%
@@ -103,7 +82,7 @@ server <- function(input, output){
     
     ggplot() + geom_sf(data=land,fill=colland,col=borderland,size=0.1)+
       geom_sf(data=islands,fill=colland,col="ivory3") +
-      geom_sf(data=character_duration,aes(size=duration), color=input$color)+
+      geom_sf(data=character_duration,aes(size=duration), color='red')+
       scale_size_area("Time on screen",breaks = c(0,30,60,120,240))+
       theme_minimal()+coord_sf(expand = 0,ndiscr = 0)+
       theme(panel.background = element_rect(fill = colriver,color=NA)) +
@@ -111,59 +90,7 @@ server <- function(input, output){
     
   })
   
-  output$map2 <- renderPlot({
-    
-    jstime = appearances %>% filter(name==input$names1) %>% 
-      left_join(scenes) %>% 
-      group_by(episodeId) %>% 
-      summarise(time=sum(duration))
-    
-    ggplot(jstime) + 
-      geom_line(aes(x=episodeId,y=time), color = input$color1, size = 1)+
-      theme_bw()
-  
-  })
-  
-  
-  output$map3 <- renderPlot({
-    
-    scenes_stats=scenes %>% left_join(episodes) %>% 
-      group_by(episodeTitle,seasonNum) %>% 
-      summarize(nb_scenes=n(),duration_max=max(duration),nbdeath=sum(nbdeath))
-    
-    labels = scenes_stats %>% filter(duration_max>400|nb_scenes>200)
-    
-    ggplot(scenes_stats,aes(x=nb_scenes,y=duration_max,col=factor(seasonNum)))+
-      geom_point(aes(size=nbdeath))+
-      geom_text(data=labels,aes(label=episodeTitle),vjust=-0.6)+
-      scale_x_continuous("Number of scene",limits = c(0,280))+
-      scale_y_continuous("The duration of the longest scene",limits = c(100,800))+
-      scale_color_brewer("season",palette ="Spectral")+
-      guides(colour = "legend", size = "legend")+
-      theme_bw()    
-    
-  })
-  
-
-    observeEvent(input$aPropos, {
-    shinyalert(
-      title = "About",
-      text = readLines("www/a-propos.html"),
-      size = "s",
-      closeOnEsc = TRUE,
-      closeOnClickOutside = TRUE,
-      html = TRUE,
-      type = "info",
-      showConfirmButton = TRUE,
-      showCancelButton = FALSE,
-      confirmButtonText = "OK",
-      confirmButtonCol = "#AEDEF4",
-      timer = 0,
-      imageUrl = "",
-      animation = TRUE
-    )
-  })
-  
 }
+
 
 shinyApp(ui = ui, server = server)
