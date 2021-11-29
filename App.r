bf5924305fa8d99bc4aa44395cc2090b3b15711c
AchrafBella <achraf.bella79@gmail.com>
2021-11-29 20:54:59 +0100

    last v

--- App.r
+++ App.r
@@ -7,23 +7,25 @@ library(ggplot2)
 library(roxygen2)
 library(ggiraph)
 
-
 source("App_functions.R")
 source("data_acquisition.R")
 
+character_name_list <- appearances %>% pull(name) %>% unique()
 
 ui <- dashboardPage(
   dashboardHeader(title="Game of Thrones data Analysis"),
   
   dashboardSidebar(
     br(),
-    selectInput("saison", "Choose a season", 
-                choices = c(1, 2, 3, 4, 5, 6, 7, 8),),
-  selectInput("episode", "Choose an Episode",
+    checkboxGroupInput(inputId = "seasonC",label = "Seasons",
+                       choices = c(1,2,3,4,5,6,7,8) ,selected =season),
+    selectInput("season", "Choose a season", 
+                choices = c(1,2,3,4,5,6,7,8),),
+    selectInput("episode", "Choose an Episode",
               choices = c(1,2,3,4,5),),
-  radioButtons("ds", "See the place of :",
-    choices = c("Scenes", "Death people")),
-  actionButton("btn1", "Afficher"),
+    radioButtons("ds", "See the place of :",
+                 choices = c("Scenes", "Death people")),
+    actionButton("btn1", "Afficher"),
   
   selectInput("names", "Choose character name:",character_name_list),
 
@@ -51,9 +53,11 @@ ui <- dashboardPage(
                         h2("title to be added"),
                         plotOutput("displayMap", width = "100%")),
                
-               tabPanel("Time series data",
-                        h2("title to be added"),
-                        plotOutput(outputId="g", width="100%")),
+               tabPanel("Analysis",
+                        h2("Statistics on GOT data"),
+                        plotOutput(outputId="g1", width="100%"),
+                        plotOutput(outputId="g2", width="100%"),
+                        plotOutput(outputId="g3", width="100%")),
                
                tabPanel("Data", 
                         DT::dataTableOutput('rawtable')),
@@ -68,11 +72,11 @@ ui <- dashboardPage(
 
 server <- function(input, output){
   
-  output$displayMap <- renderPlot({
-    got_map()
-  })
+  output$displayMap <- renderPlot({})
 
-  output$g <- renderPlot({ get_time_spent_per_character(input$names) })
+  output$g1 <- renderPlot({ function1(input$seasonC) })
+  output$g2 <- renderPlot({ function2(input$seasonC) })
+  output$g3 <- renderPlot({ function3(input$seasonC) })
   
   output$rawtable <- DT::renderDataTable(read_csv(
     file.path('data',paste(input$df,'.csv', sep=''))))
--- App_functions.R
+++ App_functions.R
@@ -1,3 +1,6 @@
+main_char= c("Jon Snow", "Tyrion Lannister","Daenerys Targaryen",
+             "Sansa Stark","Cersei Lannister","Arya Stark")
+
 
 get_time_spent_per_character <- function(char_name){
   
@@ -12,8 +15,67 @@ get_time_spent_per_character <- function(char_name){
   
 }
 
-character_name_list <- appearances %>% pull(name) %>% unique()
 
+function1 <- function(season){
+  
+  screenTimePerSeasons = appearances %>% left_join(scenes) %>% 
+    left_join(episodes) %>% 
+    group_by(name,seasonNum) %>%  filter(name %in% main_char)%>%  filter(seasonNum %in% season)%>% 
+    summarise(screenTime=sum(duration)) %>% 
+    arrange(desc(screenTime)) 
+  screenTimeTotal = screenTimePerSeasons %>% 
+    group_by(name) %>% 
+    summarise(screenTimeTotal=sum(screenTime))
+  
+  mainCharacters = screenTimeTotal %>% 
+    filter(screenTimeTotal>60*60) %>% 
+    arrange(screenTimeTotal) %>% 
+    mutate(nameF=factor(name,levels = name))
+  data = screenTimePerSeasons %>% left_join(mainCharacters) %>% filter(!is.na(nameF))
+  ggplot(data)+
+    geom_bar(aes(y=nameF,x=screenTime/60,fill=factor(seasonNum,level=8:1)),
+             stat="identity")+
+    scale_fill_brewer("Saison",palette = "Spectral")+theme_bw()+
+    geom_text(data=mainCharacters,aes(y=nameF,x=screenTimeTotal/60+5,
+                                      label=paste(round(screenTimeTotal/60),'min')),hjust = "left")+
+    scale_x_continuous("Temps d'apparition (min)",
+                       breaks = seq(0,750,by=120),limits = c(0,780),expand = c(0,1))+
+    ylab("")+ggtitle("Temps d'apparition cumulé par personnage et saison")
+}
+
+
+
+function2 <- function(season){
+  
+  labels = scenes %>% filter(duration>400)
+  ggplot(scenes %>% left_join(episodes)%>% filter(seasonNum %in% season))+
+    geom_boxplot(aes(x=factor(episodeId),y=duration,fill=factor(seasonNum)))+
+    geom_text(data=labels ,aes(x=factor(episodeId),y=duration,label=subLocation),hjust = "right",vjust="top")+
+    scale_x_discrete("N° épisode",as.character(seq(1,73, by=5)))+
+    scale_fill_brewer(palette="Spectral",guide="none")+
+    ylab("Durée des scènes (min)")+
+    ggtitle("Répartition des durées des scènes par épisodes")+
+    theme_bw()
+  
+}
+
+function3 <- function(season){
+  
+  scenes_stats=scenes %>% left_join(episodes) %>% 
+    group_by(episodeTitle,seasonNum) %>% filter(seasonNum %in% season) %>% 
+    summarize(nb_scenes=n(),duration_max=max(duration),nbdeath=sum(nbdeath))
+  
+  labels = scenes_stats %>% filter(duration_max>400|nb_scenes>200|seasonNum==1)
+  ggplot(scenes_stats,aes(x=nb_scenes,y=duration_max,col=factor(seasonNum)))+
+    geom_point(aes(size=nbdeath))+
+    geom_text(data=labels,aes(label=episodeTitle),vjust=-0.6)+
+    scale_x_continuous("Nombre de scène",limits = c(0,280))+
+    scale_y_continuous("Durée de la scène la plus longue",limits = c(100,300))+
+    scale_color_brewer("Saison",palette ="Spectral")+
+    guides(colour = "legend", size = "legend")+
+    ggtitle("informations sur les saisons et le nombre de morts ")+
+    theme_bw()
+}
 
 got_map <- function(){
   borderland = "ivory3"  
@@ -25,55 +87,3 @@ got_map <- function(){
     theme(panel.background = element_rect(fill = colriver,color=NA)) +
     labs(title = "GoT",caption = "Etiennne C�me, 2020",x="",y="")
 }
-
-
-continents$type="continent"
-islands$type="island"
-lakes$type="lake"
-rivers$type="river"
-roads$type="road"
-wall$type="wall"
-
-allDatas = bind_rows( continents,islands, landscape, rivers,lakes,roads,wall)
-
-spaces = c(
-  "continent",
-  "forest",
-  "mountain",
-  "stepp",
-  "swamp",
-  "lake",
-  "river",
-  "road",
-  "island",
-  "location",
-  "wall",
-  "political",
-  "desert",
-  "land",
-  "shore",
-  "water"
-)
-
-cols = c(
-  "ivory",
-  "green",
-  "gray88",
-  "#669933",
-  "cyan4",
-  "blue",
-  "cyan3",
-  "darkgray",
-  "gold",
-  "black",
-  "gold4",
-  "orangered3",
-  "darkgoldenrod1",
-  "gray",
-  "yellow",
-  "#33CCFF"
-)
-
-
-names(cols) = spaces
-levels(allDatas$type) = spaces
